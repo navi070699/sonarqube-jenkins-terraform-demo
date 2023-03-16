653e35339c1cebb004bfe6492fd860d6e47ba07c
naveen <naveen@gmail.com>
2023-03-16 16:10:07 +0530

    dergerg

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.58.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
-
 provider "aws" {
-  # Configuration options
+  region  = "us-east-1"
+  profile = "default"
 }
